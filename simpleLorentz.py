c69f45fdbf8f2ab556ca0b9566cf9568ba46a045
ownedbyphysics <48797734+ownedbyphysics@users.noreply.github.com>
2022-01-11 18:20:31 +0200

    Update simpleLorentz.py

--- simpleLorentz.py
+++ simpleLorentz.py
@@ -1,15 +1,25 @@
 import pygame
 import random
 
+initials_dictionary = { 'sizeX' : 20,
+                     'sizeY' : 20,
+                     'sizeZ' : 50,
+                     'X' : 0.1,
+                     'Y' : 0,
+                     'Z' : 0
+                    }
+
 class Lorentz_equations:
     def __init__(self):
-        self.xmin, self.xmax = -30, 30
-        self.ymin, self.ymax = -30, 30
-        self.zmin, self.zmax =  0, 20
-        self.X, self.Y, self.Z = 0.1, 0.0, 0.0
+        self.xmin, self.xmax = -initials_dictionary['sizeX'], initials_dictionary['sizeX']
+        self.ymin, self.ymax = -initials_dictionary['sizeY'], initials_dictionary['sizeY']
+        self.zmin, self.zmax = -initials_dictionary['sizeZ']+30, initials_dictionary['sizeZ']
+        self.X, self.Y, self.Z = initials_dictionary['X'], initials_dictionary['Y'], initials_dictionary['Z']
         self.ghostX, self.ghostY, self.ghostZ = self.X, self.Y, self.Z
-        self.dt = 0.00001
-        self.sigma, self.ro, self.beta = 10, 28, 8/3
+        self.dt = 0.0005
+        self.sigma = 10 
+        self.ro = 28 
+        self.beta = 3
         self.pixelwidth = 1
         
     def loop(self):
@@ -21,14 +31,14 @@ class Lorentz_equations:
     def draw(self, displaySurface):
         width, height = displaySurface.get_size()
         
-        before = self.show(self.ghostX, self.ghostY,
+        before = self.show(self.ghostX, self.ghostZ,
                                       self.xmin, self.xmax,
-                                      self.ymin, self.ymax,
+                                      self.zmin, self.zmax,
                                       width, height)
         
-        after = self.show(self.X, self.Y,
+        after = self.show(self.X, self.Z,
                                       self.xmin, self.xmax,
-                                      self.ymin, self.ymax,
+                                      self.zmin, self.zmax,
                                       width, height)
         
         updateGraph = pygame.draw.line(displaySurface, 
@@ -52,7 +62,7 @@ class Attractors_app:
         self.displaySurface = None
         self.fpsClock = None
         self.attractors = []
-        self.canvasDimension = self.width, self.height = 1400, 800
+        self.canvasDimension = self.width, self.height = 1200, 800
         self.count = 0
         self.outputCount = 1
         self.file = 'Michael Nyman - The heart asks for pleasure first.mp3'
@@ -66,10 +76,10 @@ class Attractors_app:
         self.fpsClock = pygame.time.Clock()
         
         colour = []
-        colour.append((50, 130, 200))
-        colour.append((255, 130, 0))
-        colour.append((255, 200, 0))
-        
+        colour.append((255,215,0))
+        colour.append((255,64,64))
+        colour.append((0,178,238))
+    
         for trajectory in range(0,3):
             self.attractors.append(Lorentz_equations())
             self.attractors[trajectory].X = random.uniform(0.0, 0.01)
@@ -111,4 +121,4 @@ class Attractors_app:
 if __name__ == "__main__":
     app = Attractors_app()
     app.startMe()
-    
+        
